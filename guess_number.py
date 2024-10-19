0f631f6db645ba4c64bdb153cafeb120ee794464
STAFFOVICH <sweetsstream@gmail.com>
2024-10-20 00:24:36 +0300

    Update guess_number.py

--- guess_number.py
+++ guess_number.py
@@ -1,10 +1,11 @@
 import random
 
+
 def guess_number():
     # Выбираем случайное число от 1 до 100
     number = random.randint(1, 100)
 
-    print("Я загадал число от 1 до 100.")
+    print("Я загадал число от 1 до 100 за 6 попыток. ")
 
     for _ in range(6):  # Разрешаем пользователю сделать 6 попыток
         guess = int(input("Попробуйте угадать число: "))
@@ -18,3 +19,6 @@ def guess_number():
             print("Слишком большое число.")
     else:
         print("Вы использовали все попытки. Число было", number)
+
+
+guess_number()
